f0de64a34adec35f38072a705869ff9ba60a8c56
Administrator <897737156@qq.com>
2016-08-11 17:29:49 +0800

    aa

--- mine/src/main/java/com/example/mine/Mind.java
+++ mine/src/main/java/com/example/mine/Mind.java
@@ -1,12 +1,19 @@
 package com.example.mine;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Administrator on 2016/8/11 0011.
  */
 public class Mind extends Fragment {
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
